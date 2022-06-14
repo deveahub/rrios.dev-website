6bc88956d071e578110343ef276f8437ce951a04
rriosper <roberto.rios.perea@outlook.com>
2022-06-14 21:13:27 +0000

    Deploying to gh-pages from @ rrios-dev/rrios.dev-website@0260175bd30dc6ceea6258d8104bf5a5c4f5cfde 🚀

---
+++ _next/static/epaveU6hQk2ddY_I2fMS7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/147-853b88667c126738.js","static/chunks/pages/index-116162f74276041e.js"],"/_error":["static/chunks/pages/_error-6e6353f79838677c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
